a614cf35b99719810d20c41d1d2fd360951cf6d1
Johannes Filter <filter@st.ovgu.de>
2013-03-26 19:01:58 +0100

    finished first assignements

--- progfun-recfun/src/main/scala/recfun/Main.scala
+++ progfun-recfun/src/main/scala/recfun/Main.scala
@@ -14,15 +14,38 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int =
+    if (c == 0) 1
+    else if (c > r) 0
+    else pascal(c, r - 1) + pascal(c - 1, r - 1)
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+
+    def balance(chars: List[Char], n: Int): Boolean =
+      if (n < 0) false
+      else if (chars.isEmpty)
+        n == 0
+      else
+        balance(chars.tail, changeN(chars.head, n))
+
+    def changeN(c: Char, n: Int): Int =
+      if (c == '(') n + 1
+      else if (c == ')') n - 1
+      else n
+
+    balance(chars, 0)
+  }
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int =
+    if (coins.isEmpty) 0
+    else if (money == 0) 1
+    else if (money < 0) 0
+    else countChange(money - coins.head, coins) + countChange(money, coins.tail)
+
 }
